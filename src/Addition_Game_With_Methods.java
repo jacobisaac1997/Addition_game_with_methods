f05afac4e9f30efb2d445bdfa5e29c48da6b08b2
Jacob Isaac <isaacj@student.swosu.edu>
2015-11-11 09:40:13 -0600

    added some code

--- src/Addition_Game_With_Methods.java
+++ src/Addition_Game_With_Methods.java
@@ -1,13 +1,21 @@
 
 public class Addition_Game_With_Methods {
 	public static void main(String[] args) {
-		int randomNumber1 = 0;
-		randomNumber1 = randomNumberGenerator();
-		System.out.println(randomNumber1);
+		additionGameMethod();
 	}
 	public static int randomNumberGenerator(){
 		int randomNumber1 = 0;
-		randomNumber1= (int)(Math.random() * 10);
-		return randomNumber1;
+		int randomNumber2 = 0;
+		randomNumber1 = (int)(Math.random() * 10);
+		randomNumber2 = (int)(Math.random() * 10);
+		return randomNumber1 + randomNumber2;
+	}
+	public static void additionGameMethod(){
+		randomNumberGenerator();
+		int randomNumber1 = 0;
+		int randomNumber2 = 0;
+		for(int roundNumber = 0; roundNumber<2; roundNumber++){
+			System.out.println("What is " + randomNumber1 + " + "  + randomNumber2 +" ?");
+		}
 	}
 }
