7630253377c401d92026b1b7a931ceee30888f9e
Jacob Isaac <isaacj@student.swosu.edu>
2015-11-11 09:21:09 -0600

    Added rng

--- src/Addition_Game_With_Methods.java
+++ src/Addition_Game_With_Methods.java
@@ -1,12 +1,13 @@
 
 public class Addition_Game_With_Methods {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int randomNumber1 = 0;
+		randomNumber1 = randomNumberGenerator();
+		System.out.println(randomNumber1);
 	}
-
+	public static int randomNumberGenerator(){
+		int randomNumber1 = 0;
+		randomNumber1= (int)(Math.random() * 10);
+		return randomNumber1;
+	}
 }
